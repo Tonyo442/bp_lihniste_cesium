b22ef2fee2b26dddda2faa167f9a3d185214b3bf
Tonyo442 <antonin.skacel04@gmail.com>
2026-03-13 21:54:45 +0100

    Modify cesiumAccessToken and targetLocation
    Updated target location coordinates and altitude.

--- js/cesiumConfig.js
+++ js/cesiumConfig.js
@@ -1,7 +1,7 @@
 const cesiumAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ZGZhNzA0YS1jNmEyLTQ4ZmMtYTU2OS1hNGRlMTI3YzVjYmYiLCJpZCI6MzY2Njg2LCJpYXQiOjE3NzMxNDc4MzF9.r1cFVZ_Js7D46zXy-hW7sq9M28aYMz-5dkfQXPVwA9A';
 
  const targetLocation = {
-    destination: Cesium.Cartesian3.fromDegrees(17.1406, 49.6748, 300),
+    destination: Cesium.Cartesian3.fromDegrees(17.1403, 49.6765, 400),
     orientation: {
     heading: Cesium.Math.toRadians(0.0),
     pitch: Cesium.Math.toRadians(-15.0),
