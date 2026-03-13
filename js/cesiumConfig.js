f4a6236e18c78127332882123debd67dbf1497e0
Tonyo442 <antonin.skacel04@gmail.com>
2026-03-13 21:35:10 +0100

    Modify targetLocation coordinates in cesiumConfig.js
    Updated targetLocation coordinates for better accuracy.

--- js/cesiumConfig.js
+++ js/cesiumConfig.js
@@ -1,7 +1,7 @@
 const cesiumAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ZGZhNzA0YS1jNmEyLTQ4ZmMtYTU2OS1hNGRlMTI3YzVjYmYiLCJpZCI6MzY2Njg2LCJpYXQiOjE3NzMxNDc4MzF9.r1cFVZ_Js7D46zXy-hW7sq9M28aYMz-5dkfQXPVwA9A';
 
  const targetLocation = {
-    destination: Cesium.Cartesian3.fromDegrees(17.1395, 49.6777, 500),
+    destination: Cesium.Cartesian3.fromDegrees(17.1399, 49.6756, 500),
     orientation: {
     heading: Cesium.Math.toRadians(0.0),
     pitch: Cesium.Math.toRadians(-15.0),
